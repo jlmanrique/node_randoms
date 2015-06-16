b07699941777bb243534099ffc28a9908941646c
Jose Luis Manrique Cabana <kenshin2273@gmail.com>
2015-06-16 17:02:11 -0500

    Actualizacion del ejercicio de randoms

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var Estadistica = require('./utils/estadistica');
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 
 var numero_personas = getRandomInt(100,200);
-var personas = [];
 
+var personas = [];
 for (var i = 0; i < numero_personas; i++) {
-  personas.push(new Persona());  
+  personas[i] = new Persona();
 };
 
 console.log('El promedio de las edades es %d', Estadistica.promedio(personas));
